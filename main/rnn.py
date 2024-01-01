b16499a2c401968da567dc84c4ca7ecf9cf7c80b
astachu <anna.stachurka.stud@pw.edu.pl>
2024-01-01 23:51:31 +0100

    feat: add simple rnn

--- main/rnn.py
+++ main/rnn.py
@@ -79,7 +79,7 @@ class DefaultRNN(nn.Module):
         )
 
         self.ln1 = nn.Sequential(
-            nn.Linear(128, 64), nn.BatchNorm1d(64), nn.Dropout(0.5)
+            nn.Linear(hidden_size, 64), nn.BatchNorm1d(64), nn.Dropout(0.5)
         )
 
         self.ln2 = nn.Sequential(nn.Linear(64, 32), nn.BatchNorm1d(64), nn.Dropout(0.5))
@@ -105,5 +105,41 @@ class DefaultRNN(nn.Module):
         # print(x.shape)
         x = x[:, 0, :]
         # print(x.shape)
+
+        return x
+
+
+class SimpleRNN(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size):
+        super(SimpleRNN, self).__init__()
+
+        class GetRNNOutput(nn.Module):
+            def forward(self, x):
+                out, _ = x
+                return out
+
+        self.rnn1 = nn.Sequential(
+            nn.RNN(input_size=input_size, hidden_size=hidden_size, batch_first=True),
+            GetRNNOutput(),
+        )
+
+        self.ln1 = nn.Sequential(
+            nn.Linear(hidden_size, 128), nn.ReLU(), nn.Dropout(0.3)
+        )
+        self.ln2 = nn.Sequential(nn.Linear(128, 64), nn.ReLU(), nn.Dropout(0.3))
+
+        self.fc1 = nn.Linear(64, 32)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(32, num_classes)
+
+    def forward(self, x):
+        x = x.squeeze(1)
+        x = self.rnn1(x)
+        x = self.ln1(x)
+        x = self.ln2(x)
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = x[:, 0, :]
 
         return x
